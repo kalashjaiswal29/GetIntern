33c27a6129564e61c9f7df6b4a8d40a06c4d928b
kalashjaiswal29 <kalashjaiswal57@gmail.com>
2026-03-01 22:48:55 +0530

    react analytics update

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import Footer from "./components/footerPage8.jsx";
 import SocialPopup from "./components/SocialPopup.jsx"
 import OfferBox from "./components/offerBox.jsx"
 import MovingTicker from "./components/movingTicker.jsx"
-import { Analytics } from "@vercel/analytics/next"
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (<>
     <Analytics />

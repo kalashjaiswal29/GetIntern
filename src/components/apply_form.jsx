be664def4ed174d3dcf3ad756c12ee42e1af12fc
kalashjaiswal29 <kalashjaiswal57@gmail.com>
2026-05-30 11:00:37 +0530

    date update

--- src/components/apply_form.jsx
+++ src/components/apply_form.jsx
@@ -91,10 +91,10 @@ const ApplyForm = () => {
   ];
 
   const batchDates = [
-    "29 May 2026",
     "5 June 2026",
     "12 June 2026",
     "19 June 2026",
+    "26 June 2026"
   ];
   useEffect(() => {
     const handleClickOutside = (event) => {
